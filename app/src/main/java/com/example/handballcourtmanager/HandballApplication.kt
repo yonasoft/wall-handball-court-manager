08e553f0ab18d911ca021e3665769ef1cb19701c
KevC000 <chenkevin1119@gmail.com>
2022-07-22 10:55:39 -0400

    Refactored for repository changes

--- app/src/main/java/com/example/handballcourtmanager/HandballApplication.kt
+++ app/src/main/java/com/example/handballcourtmanager/HandballApplication.kt
@@ -2,6 +2,7 @@ package com.example.handballcourtmanager
 
 import android.app.Application
 
+//Application context for this app
 class HandballApplication:Application() {
     override fun onCreate() {
         super.onCreate()
