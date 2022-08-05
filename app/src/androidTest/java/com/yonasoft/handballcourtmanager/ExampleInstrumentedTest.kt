7a0b4a3c81718b16af622c051bc9a70485168eea
KevC000 <chenkevin1119@gmail.com>
2022-08-05 11:27:00 -0400

    changed package name

--- app/src/androidTest/java/com/yonasoft/handballcourtmanager/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yonasoft/handballcourtmanager/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.handballcourtmanager", appContext.packageName)
+        assertEquals("com.yonasoft.handballcourtmanager", appContext.packageName)
     }
 }
