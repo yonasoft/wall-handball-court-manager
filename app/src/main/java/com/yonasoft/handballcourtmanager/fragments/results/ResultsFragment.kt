00e57c8ab904380bed9b8c646a5d8ea2c77737e9
KevC000 <chenkevin1119@gmail.com>
2023-08-16 16:16:24 -0400

    results screen

--- app/src/main/java/com/yonasoft/handballcourtmanager/fragments/results/ResultsFragment.kt
+++ app/src/main/java/com/yonasoft/handballcourtmanager/fragments/results/ResultsFragment.kt
@@ -12,29 +12,35 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yonasoft.handballcourtmanager.R
+import com.yonasoft.handballcourtmanager.adapter.MatchesAdapter
 import com.yonasoft.handballcourtmanager.databinding.FragmentResultsBinding
 import com.yonasoft.handballcourtmanager.db.matchesdb.Match
 import com.yonasoft.handballcourtmanager.fragments.matches.viewmodel.MatchesViewModel
 import com.google.android.material.snackbar.Snackbar
-import com.yonasoft.handballcourtmanager.adapter.MatchesAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ResultsFragment : Fragment() {
 
-    private var binding: FragmentResultsBinding?=null
+    private var binding: FragmentResultsBinding? = null
     private val viewModel: MatchesViewModel by viewModels()
-    private val adapter:MatchesAdapter by lazy {MatchesAdapter()}
+    private val adapter: MatchesAdapter by lazy { MatchesAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_results, container, false)
+        setupMenuProvider()
+        setupRecyclerView()
 
-        val menuHost: MenuHost =requireActivity()
-        menuHost.addMenuProvider(object:MenuProvider{
+        return binding?.root
+    }
+
+    private fun setupMenuProvider() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                 menuInflater.inflate(R.menu.results_toolbar, menu)
             }
@@ -43,70 +49,59 @@ class ResultsFragment : Fragment() {
                 onDeleteMatches(menuItem.itemId)
                 return true
             }
-        }
-            ,viewLifecycleOwner, Lifecycle.State.RESUMED
-            )
-
-        setupRecyclerView()
-
-        return binding!!.root
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
-
     private fun onDeleteMatches(idOfQueueDeletion: Int) {
-        val removedQueueText: String
-        val removedList = mutableListOf<Match>()
-
-        when (idOfQueueDeletion) {
+        val removedQueueText = when (idOfQueueDeletion) {
             R.id.clear_results -> {
-                removedList.addAll(viewModel.resultsList.value!!)
-                removedQueueText = "Matches are being cleared!"
+                val removedList = viewModel.resultsList.value?.toMutableList() ?: mutableListOf()
                 viewModel.removeAllResults()
+                "Matches are being cleared!"
             }
-            else-> return
+            else -> return
         }
 
-        val snackBar =
-            Snackbar.make(binding!!.root, removedQueueText, Snackbar.LENGTH_LONG).setAction(
-                "Undo"
-            ) {
-                viewModel.addMatches(removedList)
-            }
-        snackBar.show()
+        Snackbar.make(binding?.root ?: return, removedQueueText, Snackbar.LENGTH_LONG).setAction(
+            "Undo"
+        ) {
+            viewModel.resultsList.value?.let { viewModel.addMatches(it) }
+        }.show()
     }
 
     private fun setupRecyclerView() {
         val layoutManager = LinearLayoutManager(this.context)
         layoutManager.orientation = RecyclerView.VERTICAL
         binding!!.rcvResults.layoutManager = layoutManager
+        binding!!.rcvResults.setupSwipeToDelete()
         viewModel.resultsList.observe(viewLifecycleOwner) {
             adapter.setData(it)
             binding!!.rcvResults.adapter = adapter
         }
 
+    }
+
+    private fun RecyclerView.setupSwipeToDelete() {
         ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
             override fun onMove(
                 recyclerView: RecyclerView,
                 viewHolder: RecyclerView.ViewHolder,
                 target: RecyclerView.ViewHolder
-            ): Boolean {
-                return false
-            }
+            ): Boolean = false
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                val removedMatch: Match = viewModel.resultsList.value!![viewHolder.adapterPosition]
-                viewModel.removeMatch(removedMatch)
+                val position = viewHolder.adapterPosition
+                val removedMatch = viewModel.resultsList.value?.get(position) ?: return
 
+                viewModel.removeMatch(removedMatch)
                 Snackbar.make(
-                    binding!!.root, "Result is being removed. press 'Undo' to stop!",
+                    this@setupSwipeToDelete, "Result is being removed. press 'Undo' to stop!",
                     Snackbar.LENGTH_LONG
-                ).setAction(
-                    "Undo"
-                ) {
+                ).setAction("Undo") {
                     viewModel.addMatch(removedMatch)
                 }.show()
             }
-        }).attachToRecyclerView(binding!!.rcvResults)
+        }).attachToRecyclerView(this)
     }
 
     override fun onDestroy() {
