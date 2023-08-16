8c6441acaecd2a6e94393ce83c418a9a9ef504a8
KevC000 <chenkevin1119@gmail.com>
2023-08-16 14:15:33 -0400

    refactored main activity

--- app/src/main/java/com/yonasoft/handballcourtmanager/MainActivity.kt
+++ app/src/main/java/com/yonasoft/handballcourtmanager/MainActivity.kt
@@ -3,50 +3,25 @@ package com.yonasoft.handballcourtmanager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.yonasoft.handballcourtmanager.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-//Change colors for results, matches, players, and list items.
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private var binding:ActivityMainBinding?=null
+    private var binding: ActivityMainBinding? = null
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navController:NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setup data binding
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        //Sets up navigation
+        setupDataBinding()
         setupNavigation()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding = null
-    }
-
-    //Sets up navigation
-    private fun setupNavigation() {
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        setSupportActionBar(binding!!.toolbar)
-        //Instantiate app ba
-        appBarConfiguration = AppBarConfiguration(setOf(R.id.rosterFragment,R.id.matchesFragment,R.id.resultsFragment))
-        //Navigation for tool bar
-        binding!!.toolbar.setupWithNavController(navController,appBarConfiguration)
-        //Navigation for bottom nav bar
-        binding!!.bottomNav.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,15 +30,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return  item.onNavDestinationSelected(findNavController(binding!!.navHostFragment.id))||super.onOptionsItemSelected(item)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
-                || super.onSupportNavigateUp()
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    private fun setupDataBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private fun setupNavigation() {
+        setupNavController()
+        setupActionBar()
+        setupBottomNav()
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding?.toolbar)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.rosterFragment, R.id.matchesFragment, R.id.resultsFragment))
+        binding?.toolbar?.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    private fun setupBottomNav() {
+        binding?.bottomNav?.setupWithNavController(navController)
+    }
+
 }
-
-
-
-
